18c06f32bc3db6b6953236f27c4f2869e2cd833d
Abraham Hosch <hosch250@live.com>
2017-01-02 18:57:59 -0600

    Make the move history support a player moving multiple jumps one at a time.

--- Checkers/PublicAPI.fs
+++ Checkers/PublicAPI.fs
@@ -15,27 +15,46 @@ let isValidMove startCoord endCoord gameController =
     | None -> true
     | coord -> startCoord = coord.Value
 
-let internal getGameHistory (currentGameHistory :PDNTurn List) player move startBoard endBoard =
+let internal getGameHistory (currentGameHistory :PDNTurn List) player isContinuedMove move board =
     let pdnMove = (List.map (fun item -> (square item PDNBoard).Value) move)
     let newTurnValue =
-        match player with
-        | Black ->
+        match player, isContinuedMove with
+        | Black, true ->
             let moveNumber = currentGameHistory.Length + 1
             {
                 MoveNumber = moveNumber;
-                BlackMove = { Move = pdnMove; ResultingFen = (createFen player endBoard)};
+                BlackMove = { Move = pdnMove; ResultingFen = (createFen player board)};
                 WhiteMove = None;
                 DisplayString = moveNumber.ToString() + ": " + String.Join((if isJump move then "x" else "-"), pdnMove)
             }
-        | White ->
+        | White, true ->
             let lastMovePDN = List.last currentGameHistory
             let moveNumber = currentGameHistory.Length
             {
                 MoveNumber = moveNumber;
                 BlackMove = lastMovePDN.BlackMove;
-                WhiteMove = Some { Move = pdnMove; ResultingFen = (createFen player endBoard)};
+                WhiteMove = Some { Move = pdnMove; ResultingFen = (createFen player board)};
                 DisplayString = moveNumber.ToString() + ": " + lastMovePDN.DisplayString + ", " + String.Join((if isJump move then "x" else "-"), pdnMove)
             }
+        | Black, false ->
+            let lastMovePDN = List.last currentGameHistory
+            let newPDNMove = lastMovePDN.BlackMove.Move @ pdnMove
+            {
+                MoveNumber = lastMovePDN.MoveNumber;
+                BlackMove = { Move = newPDNMove; ResultingFen = (createFen player board)};
+                WhiteMove = None;
+                DisplayString = lastMovePDN.MoveNumber.ToString() + ": " + String.Join((if isJump move then "x" else "-"), newPDNMove)
+            }
+        | White, false ->
+            let lastMovePDN = List.last currentGameHistory
+            let newPDNMove = lastMovePDN.WhiteMove.Value.Move @ pdnMove
+            {
+                MoveNumber = lastMovePDN.MoveNumber;
+                BlackMove = lastMovePDN.BlackMove;
+                WhiteMove = Some { Move = newPDNMove; ResultingFen = (createFen player board)};
+                DisplayString = lastMovePDN.MoveNumber.ToString() + ": " + lastMovePDN.DisplayString.Split(',').[0] + ", " + String.Join((if isJump move then "x" else "-"), newPDNMove)
+            }
+            
 
     match player with
     | Black -> currentGameHistory @ [newTurnValue]
@@ -56,7 +75,7 @@ let movePiece startCoord endCoord gameController :Option<GameController> =
                 CurrentCoord = match playerTurnEnds [startCoord; endCoord] gameController.Board b with
                                 | true -> None
                                 | false -> Some endCoord
-                MoveHistory = getGameHistory gameController.MoveHistory gameController.CurrentPlayer [startCoord; endCoord] gameController.Board b
+                MoveHistory = getGameHistory gameController.MoveHistory gameController.CurrentPlayer (gameController.CurrentCoord <> None) [startCoord; endCoord] b
             }
 
 let move (move :Coord seq) (gameController) :Option<GameController> =
@@ -74,7 +93,7 @@ let move (move :Coord seq) (gameController) :Option<GameController> =
                 CurrentCoord = match playerTurnEnds moveAsList gameController.Board b with
                                 | true -> None
                                 | false -> Some (Seq.last move)
-                MoveHistory = getGameHistory gameController.MoveHistory gameController.CurrentPlayer moveAsList gameController.Board b
+                MoveHistory = getGameHistory gameController.MoveHistory gameController.CurrentPlayer (gameController.CurrentCoord <> None) moveAsList b
             }
 
 let getMove searchDepth gameController =
@@ -94,7 +113,8 @@ let takeBackMove gameController =
             match gameController.MoveHistory with
             | [] -> []
             | _ ->
-                let newLastMove = {(List.last gameController.MoveHistory) with WhiteMove = None}
+                let lastMove = (List.last gameController.MoveHistory)
+                let newLastMove = {lastMove with WhiteMove = None; DisplayString = lastMove.DisplayString.Split(',').[0]}
                 List.truncate (gameController.MoveHistory.Length - 1) gameController.MoveHistory @ [newLastMove]
 
     {(controllerFromFen fen) with MoveHistory = newMoveHistory}
