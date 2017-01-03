377b9b5f2269efebb806329f111c03fb28f1e46c
Abraham Hosch <hosch250@live.com>
2017-01-03 12:46:00 -0600

    Remove dead code

--- Checkers/GameController.fs
+++ Checkers/GameController.fs
@@ -2,12 +2,6 @@
 open Checkers.Types
 open Checkers.Board
 
-[<Literal>]
-let BlackSymbol = 'B'
-
-[<Literal>]
-let WhiteSymbol = 'W'
-
 type GameController = { Board :Board; CurrentPlayer :Player; CurrentCoord :Option<Coord>; MoveHistory :PDNTurn List }
 
 let newGame = { Board = Board.defaultBoard; CurrentPlayer = Black; CurrentCoord = None; MoveHistory = [] }
