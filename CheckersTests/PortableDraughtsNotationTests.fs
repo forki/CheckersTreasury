256e3f8321bf82bd4872614c5a8be48e12cc9d85
Abraham Hosch <hosch250@live.com>
2017-01-05 12:47:01 -0600

    Tweak test

--- CheckersTests/PortableDraughtsNotationTests.fs
+++ CheckersTests/PortableDraughtsNotationTests.fs
@@ -47,9 +47,12 @@ let ``Create FEN from controller string``() =
 [<Fact>]
 let ``Create controller from FEN string: white move does not crash``() =
     let fenString = "[FEN \"W:W21,22,23,24,25,26,27,28,29,30,31,32:B1,2,3,4,5,6,7,8,10,11,12,13\"]"
-    let controller = controllerFromFen fenString
-    let newController = (movePiece {Row = 5; Column = 6} {Row = 4; Column = 7} controller).Value
-    let move = newController.MoveHistory.Head
+    let controller =
+        fenString
+        |> controllerFromFen
+        |> movePiece {Row = 5; Column = 6} {Row = 4; Column = 7}
+
+    let move = controller.Value.MoveHistory.Head
     
     Assert.Equal("…", move.BlackMove.DisplayString)
     Assert.Equal("24-20", move.WhiteMove.Value.DisplayString)
